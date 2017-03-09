322d9667e7dd6526583e8526115f26abf30f81ba
Jon Myers <jlm4508@rit.edu>
2017-03-09 12:12:15 -0500

    Added Integrity Checker

---
+++ Tools/intCheck.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+import sqlite3 as lite
+import os
+import sys
+import hashlib
+import getopt
+
+
+STATUS_UNMODIFIED = 'Unmodified'
+STATUS_MODIFIED = 'WARNING: FILE MODIFIED'
+STATUS_NEW = 'New File'
+
+
+def get_hash(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def main(argv):
+    try:
+        con = lite.connect('integrity.db')
+        cur = con.cursor()
+    except lite.Error, e:
+        print("Error {}").format(e.args[0])
+        sys.exit(1)
+
+    try:
+        opts, args = getopt.getopt(argv, "hd:", ["directory="])
+    except getopt.GetoptError:
+        print("intCheck.py -d <DIRECTORY>")
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('intCheck.py -d <DIRECTORY>')
+            sys.exit()
+        elif opt in ("-d"):
+            directory = arg
+        else:
+            directory = "C:/"
+
+    for filename in os.listdir(directory):
+        full_file = directory + "/" + filename
+        fhash = get_hash(full_file)
+
+        with con:
+            con.row_factory = lite.Row
+            cur = con.cursor()
+
+            try:
+                cur.execute("SELECT * FROM Files WHERE Filename=?", (full_file,))
+            except lite.Error, e:
+                cur.execute("CREATE TABLE Files(Filename TEXT, Hash TEXT, Status TEXT)")
+                cur.execute("SELECT * FROM Files WHERE Filename=?", (full_file,))
+
+            row = cur.fetchone()
+
+            if row:
+                if row[1] == fhash:
+                    print("File {}: {}").format(filename, STATUS_UNMODIFIED)
+                    cur.execute("UPDATE Files set Status=? WHERE Hash=?",
+                                (STATUS_UNMODIFIED, fhash,))
+                else:
+                    print("File {}: {}").format(filename, STATUS_MODIFIED)
+                    cur.execute("UPDATE Files SET Status=? WHERE Hash=?",
+                                (STATUS_MODIFIED, row[1],))
+                    cur.execute("UPDATE Files SET Hash=? WHERE Hash=?",
+                                (fhash, row[1],))
+            else:
+                print("File {}: {}").format(filename, STATUS_NEW)
+                cur.execute("INSERT INTO Files(Filename, Hash, Status) VALUES(?, ?, ?)", (full_file, fhash, STATUS_NEW,))
+
+        if con:
+            con.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
